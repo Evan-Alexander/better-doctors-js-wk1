78f9112d0f8d146c09adb467ed004e3f48f26a52
Jason Brown <jasontbrown99@gmail.com>
2017-03-17 08:57:23 -0700

    add routing and apikey

--- js/doctors-interface.js
+++ js/doctors-interface.js
@@ -1,1 +1,7 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
+var apiKey = require('./../.env').apiKey;
+
+// var findDoctor = function(medicalIssue) {
+//   $('#display-results').empty();
+//   var array = resp
+// }
--- js/doctors.js
+++ js/doctors.js
@@ -1,12 +1,18 @@
 var apiKey = require('./../.env').apiKey;
-api key ... just in case: ed978d5a3c13650eae98b765d77fc837
 
-function Doctor() {
-
-}
-
-Doctor.prototype.findDoctor = function() {
+function Doctor(medicalIssue) {
 
 }
+
+Doctor.prototype.findDoctor = function(medicalIssue) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+   .then(function(result) {
+      console.log(result);
+    })
+   .fail(function(error){
+      console.log("fail");
+    });
+};
+
 
 exports.doctorModule = Doctor;
