e65335413c37279e71a958f8646a26f1b2d22e8e
Jason Brown <jasontbrown99@gmail.com>
2017-03-17 16:42:11 -0700

    make adjustments.  App now displays 20 doctors when a user submists an ailment

--- js/doctors-interface.js
+++ js/doctors-interface.js
@@ -5,25 +5,20 @@ var apiKey = require('./../.env').apiKey;
 var searchedDoctors = function(response) {
   $('#display-results').empty();
   var array = response.data;
-  var doctor_firstName = response.data[i].profile.first_name;
-  var doctor_lastName = response.data[i].profile.last_name;
-  var doctor_title = response.data[i].profile.title;
-  var doctor_image = response.data[i].profile.image_url;
-  var doctor_bio = response.data[i].profile.bio;
   console.log(array);
+  var doctorArray = [];
   for (i=0; i<array.length; i++) {
+    var doctor_firstName = response.data[i].profile.first_name;
+    var doctor_lastName = response.data[i].profile.last_name;
+    var doctor_title = response.data[i].profile.title;
+    var doctor_image = response.data[i].profile.image_url;
+    var doctor_bio = response.data[i].profile.bio;
     if (array.length === 0) {
       $('#display-results').append("There are no doctors with this specialty in your area.");
     } else {
-      $('#display-results').append("<div class='well col-md-6 doctor-space'><img src=" + doctor_image + "><h4>" + doctor_firstName + " "+ "<span>" + doctor_lastName + " " + "</span>" + doctor_title + "</h4><br><p>" + doctor_bio + "</p></div>");
+      $('#display-results').append("<div class='well col-md-6 doctor-space'><img src=" + doctor_image + "><h4>" + doctor_firstName + " "+ "<span>" + doctor_lastName + ", " + "</span>" + doctor_title + "</h4><br><p>" + doctor_bio + "</p></div>");
     }
   }
-  // for (i=0;i<array.length;i++){
-  //   for (j=0;j<3;j++){
-  //   $("#display-results").append("<li>"++" - "+  +"</li>")
-  //   }
-  // }
-
 };
 
 $(document).ready(function() {
@@ -31,7 +26,6 @@ $(document).ready(function() {
   $('#search-form').submit(function(event) {
     event.preventDefault();
     var medicalIssue = $('#search-ailment').val();
-    console.log(medicalIssue);
     $('#search-ailment').val("");
     doctor.getDoctor(medicalIssue, searchedDoctors);
   });
--- js/doctors.js
+++ js/doctors.js
@@ -5,10 +5,9 @@ function Doctor() {
 }
 
 Doctor.prototype.getDoctor = function(medicalIssue, searchedDoctors) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue +'&location=45.5231%2C-122.6765%2C%205&user_location=36.1699%2C-115.1398&skip=0&limit=2&user_key=' + apiKey)
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue +'&location=45.5231%2C-122.6765%2C%205&user_location=36.1699%2C-115.1398&skip=0&limit=20&user_key=' + apiKey)
    .then(function(response) {
      searchedDoctors(response);
-      console.log(response);
     }).fail(function(error){
       alert("fail");
     });
