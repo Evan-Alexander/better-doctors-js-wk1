1108ee8e0c8ae7ea9e370a5024e287551388d013
Jason Brown <jasontbrown99@gmail.com>
2017-03-17 14:45:56 -0700

    add variables to json search parameters

--- js/doctors-interface.js
+++ js/doctors-interface.js
@@ -5,12 +5,17 @@ var apiKey = require('./../.env').apiKey;
 var searchedDoctors = function(response) {
   $('#display-results').empty();
   var array = response.data;
+  var doctor_firstName = response.data[i].profile.first_name;
+  var doctor_lastName = response.data[i].profile.last_name;
+  var doctor_title = response.data[i].profile.title;
+  var doctor_image = response.data[i].profile.image_url;
+  var doctor_bio = response.data[i].profile.bio;
   console.log(array);
   for (i=0; i<array.length; i++) {
     if (array.length === 0) {
       $('#display-results').append("There are no doctors with this specialty in your area.");
     } else {
-      $('#display-results').append("<div class='well col-md-6 doctor-space'><img src="+response.data[i].profile.image_url+"><h4>" + response.data[i].profile.first_name + " "+ "<span>" + response.data[i].profile.last_name + " "+ "</span>" + response.data[i].profile.title + "</h4><br><p>" + response.data[i].profile.bio + "</p></div>");
+      $('#display-results').append("<div class='well col-md-6 doctor-space'><img src=" + doctor_image + "><h4>" + doctor_firstName + " "+ "<span>" + doctor_lastName + " " + "</span>" + doctor_title + "</h4><br><p>" + doctor_bio + "</p></div>");
     }
   }
   // for (i=0;i<array.length;i++){
