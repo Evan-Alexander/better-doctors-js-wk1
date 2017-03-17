745641cdef981903266e504e89364a55449f781b
Jason Brown <jasontbrown99@gmail.com>
2017-03-16 20:51:12 -0700

    add gulptask for concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var concat = require('gulp-concat');
 
 
-
-gulp.task('jsBrowserify', function() {
+gulp.task('jsBrowserify', ['concatInterface'], function() {
   return browserify({ entries: ['./js/doctor-interface.js'] })
     .bundle()
     .pipe(source('app.js'))
     .pipe(gulp.dest('./build.js'));
+});
+
+gulp.task('concatInterface', function() {
+  return gulp.src(['./js/doctor-interface.js'])
+    .pipe(concat('allConcat.js'))
+    .pipe(gulp.dest('./tmp'));
 });
