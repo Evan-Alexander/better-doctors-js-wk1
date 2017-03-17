7bea39bb6635801b32832e088c53f1a5315cfe55
Jason Brown <jasontbrown99@gmail.com>
2017-03-16 21:03:01 -0700

    add requires for gulp-util and clean task

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-
+var utilities = require('gulp-util');
+var buildProduction = utilities.env.production;
+var del = require('del');
 
 gulp.task('jsBrowserify', ['concatInterface'], function() {
   return browserify({ entries: ['./js/doctor-interface.js'] })
@@ -24,10 +26,14 @@ gulp.task('concatInterface', function() {
     .pipe(gulp.dest('./tmp'));
 });
 
-gulp.task("build", function() {
+gulp.task("build", ['clean'], function() {
   if (buildProduction) {
     gulp.start('minifyScripts');
   } else {
     gulp.start('jsBrowserify');
   }
+});
+
+gulp.task("clean", function() {
+  return del(['build', 'tmp']);
 });
